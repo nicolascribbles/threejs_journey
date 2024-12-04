bc0cc388cdb5729efad6c1c7b315c0654af28a88
Nicola Riker <39339887+nicolascribbles@users.noreply.github.com>
2024-12-03 19:41:48 -0700

    finished lesson_04

--- lesson_04/exercise/src/script.js
+++ lesson_04/exercise/src/script.js
@@ -12,7 +12,19 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+
 scene.add(mesh)
+mesh.position.set(0.7, - 0.6, 1)
+
+mesh.scale.x = 2
+mesh.scale.y = 0.5
+mesh.scale.z = 0.5
+
+mesh.rotation.y = Math.PI * 0.25
+mesh.rotation.x = Math.PI * 0.25
+
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 /**
  * Sizes
@@ -27,6 +39,9 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.y = 0
+camera.position.x = 1
+
 scene.add(camera)
 
 /**
