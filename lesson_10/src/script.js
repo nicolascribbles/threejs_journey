27067d409988b9f64303ea33398b99c74b296fba
Nicola Riker <39339887+nicolascribbles@users.noreply.github.com>
2024-12-28 14:17:31 -0700

    loading textures

--- lesson_10/src/script.js
+++ lesson_10/src/script.js
@@ -1,6 +1,24 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+const image = new Image()
+// old way / behind the scenes
+// const texture = new THREE.Texture(image)
+
+// image.onload = () => {
+//     texture.needsUpdate = true
+// }
+
+// image.src = 'textures/door/color.jpg'
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const texture = textureLoader.load('textures/door/color.jpg')
+
+// // in latest versiosn of three.js we need tp specify it by setting the  colorSpace to THREE.SRGBColorSpace
+// // texture.colorSpace = THREE.SRGBColorSpace
 /**
  * Base
  */
@@ -14,7 +32,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material =  new THREE.MeshBasicMaterial({ map: texture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
