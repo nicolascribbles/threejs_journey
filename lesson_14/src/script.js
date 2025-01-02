1a2a99b09f676f33dcb96fffcdec6c0f26e96794
Nicola Riker <39339887+nicolascribbles@users.noreply.github.com>
2025-01-02 13:33:19 -0700

    add helpers

--- lesson_14/src/script.js
+++ lesson_14/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper'
 
 /**
  * Base
@@ -107,6 +108,22 @@ gui.add(spotLight.target.position, 'y').min(-3).max(3).step(0.001).name('spot li
 
 // look at the spotlight
 scene.add(spotLight.target)
+
+// helpers
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+scene.add(hemisphereLightHelper)
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
+scene.add(directionalLightHelper)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.5)
+scene.add(pointLightHelper)
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight, 0.5)
+scene.add(spotLightHelper)
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight, 0.2)
+scene.add(rectAreaLightHelper)
 
 /**
  * Objects
