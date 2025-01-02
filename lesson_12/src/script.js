2f8ae550e4addab17ddbbf87985fffc88996b47d
Nicola Riker <39339887+nicolascribbles@users.noreply.github.com>
2025-01-02 11:55:24 -0700

    Update script.js

--- lesson_12/src/script.js
+++ lesson_12/src/script.js
@@ -52,14 +52,35 @@ fontLoader.load(
         )
         textGeo.center()
 
-        const textMaterial = new THREE.MeshMatcapMaterial({
+        const material = new THREE.MeshMatcapMaterial({
             matcap: matcapTexture
         })
 
 
-        const textMesh = new THREE.Mesh(textGeo, textMaterial)
+        const textMesh = new THREE.Mesh(textGeo, material)
 
         scene.add(textMesh)
+
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+
+        console.time('TorusGeometry')
+        for (let i=0; i<100; i++) {
+            
+            const donut = new THREE.Mesh(donutGeometry, material)
+            
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+    
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+
+            scene.add(donut)
+        }
+        console.timeEnd('TorusGeometry')
     }
 )
 
