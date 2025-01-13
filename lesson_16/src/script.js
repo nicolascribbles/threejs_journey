ee77004083515d9190e0d65629d4495f00f6a5de
Nicola Riker <39339887+nicolascribbles@users.noreply.github.com>
2025-01-12 23:50:49 -0700

    added bush material /textures

--- lesson_16/src/script.js
+++ lesson_16/src/script.js
@@ -48,7 +48,6 @@ coastSandRockARMTexture.wrapS = THREE.RepeatWrapping
 coastSandRockARMTexture.wrapT = THREE.RepeatWrapping
 
 // wall textures
-
 const wallTexture = textureLoader.load('./wall/castle_brick_broken_06_1k/castle_brick_broken_06_diff_1k.jpg')
 wallTexture.colorSpace = THREE.SRGBColorSpace
 
@@ -61,13 +60,32 @@ const wallNorGLTexture = textureLoader.load('./wall/castle_brick_broken_06_1k/ca
 const roofTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_diff_1k.jpg')
 roofTexture.colorSpace = THREE.SRGBColorSpace
 
-
 const roofARMTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_arm_1k.jpg')
 const roofDifferenceTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_diff_1k.jpg')
 const roofNorGLTexture = textureLoader.load('./roof/roof_slates_02_1k/roof_slates_02_nor_gl_1k.jpg')
-// const
-// const
-// const
+
+// bush textures
+const bushColorTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_diff_1k.jpg')
+const bushNormalTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_nor_gl_1k.jpg')
+const bushARMTexture = textureLoader.load('./bush/leaves_forest_ground_1k/leaves_forest_ground_arm_1k.jpg')
+
+bushColorTexture.colorSpace = THREE.SRGBColorSpace
+
+bushColorTexture.repeat.set(2, 1)
+bushNormalTexture.repeat.set(2, 1)
+bushARMTexture.repeat.set(2, 1)
+
+bushColorTexture.wrapS = THREE.RepeatWrapping
+bushNormalTexture.wrapS = THREE.RepeatWrapping
+bushARMTexture.wrapS = THREE.RepeatWrapping
+
+roofTexture.repeat.set(3, 1)
+roofARMTexture.repeat.set(3, 1)
+roofNorGLTexture.repeat.set(3, 1)
+
+roofTexture.wrapS = THREE.RepeatWrapping
+roofARMTexture.wrapS = THREE.RepeatWrapping
+roofNorGLTexture.wrapS = THREE.RepeatWrapping
 
 /**
  * House
@@ -165,24 +183,36 @@ house.add(door)
 
 // Bushes
 const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
-const bushMaterial = new THREE.MeshStandardMaterial()
+const bushMaterial = new THREE.MeshStandardMaterial({
+    color: 0xccffcc,
+    map: bushColorTexture,
+    normalMap: bushNormalTexture,
+    aoMap: bushARMTexture,
+    metalnessMap: bushARMTexture,
+    roughnessMap: bushARMTexture,
+    alphaMap: bushARMTexture,
+
+})
 
 const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush1.scale.set(0.5, 0.5, 0.5)
 bush1.position.set(0.8, 0.2, 2.2)
+bush1.rotation.x = -0.75
 
 const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush2.scale.set(0.25, 0.25, 0.25)
 bush2.position.set(1.4, 0.1, 2.1)
+bush2.rotation.x = -0.75
 
 const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush3.scale.set(0.4, 0.4, 0.4)
 bush3.position.set(-0.8, 0.1, 2.2)
+bush3.rotation.x = -0.75
 
 const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
 bush4.scale.set(0.15, 0.15, 0.15)
 bush4.position.set(-1, 0.05, 2.6)
-
+bush4.rotation.x = -0.75
 house.add(bush1, bush2, bush3, bush4)
 
 // Graves
