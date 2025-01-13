a39019ea99b7cc6027d17660d5e6ae9751e6e516
Nicola Riker <39339887+nicolascribbles@users.noreply.github.com>
2025-01-13 00:08:30 -0700

    add a door light to the house

--- lesson_16/src/script.js
+++ lesson_16/src/script.js
@@ -291,13 +291,18 @@ scene.add(graves)
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+const ambientLight = new THREE.AmbientLight('#86cdff', 0.275)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
+const directionalLight = new THREE.DirectionalLight('#86cdff', 1)
 directionalLight.position.set(3, 2, -8)
 scene.add(directionalLight)
+
+// Door light
+const doorLight = new THREE.PointLight('#ff7d46', 5)
+doorLight.position.set(0, 2.2, 2.5)
+house.add(doorLight)
 
 /**
  * Sizes
